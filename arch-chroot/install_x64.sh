d7d9cfc988f31066046f03e2c07b9456c972d5aa
Anibal Aguila <aa@usen.io>
2022-11-01 22:29:50 +0100

    feat: added x64 installation path

--- arch-chroot/install_x64.sh
+++ arch-chroot/install_x64.sh
@@ -7,22 +7,18 @@ echo "es_ES.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "LC_ALL=C" >> /etc/locale.conf
-echo "neo-00" > /etc/hostname
+echo "max-ign" > /etc/hostname
 echo -e "127.0.0.1       localhost\n::1             localhost\n127.0.1.1       localhost" >> /etc/hosts
 
-
 pacman -S --noconfirm sudo snapper openssh python python-pip git networkmanager neovim grub dhcpcd busybox efibootmgr sudo dhcpcd busybox networkmanager
-
 
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
-
 systemctl enable sshd
 systemctl enable NetworkManager
 
-
-grub-install --target=arm64-efi --recheck --removable --efi-directory=/boot --boot-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --recheck --removable --efi-directory=/boot --boot-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 grub-mkconfig -o /boot/EFI/BOOT/grub.cfg
 
